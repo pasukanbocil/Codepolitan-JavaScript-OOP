6978376ec2280b2d2cfcc287a4e7475adabe8bd1
pasukanbocil <ctian8787@gmail.com>
2024-03-10 15:18:35 +0700

    docs: :lipstick: E

--- object function factory/app.js
+++ object function factory/app.js
@@ -1,11 +1,3 @@
-const hex = (r, g, b) => {
-  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-};
-
-const rgb = (r, g, b) => {
-  return `rgb(${r}, ${g}, ${b})`;
-};
-
 function convertColor(r, g, b) {
   const color = {};
 
